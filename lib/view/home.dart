1df3fa0ab470724e85e8d1539be4499fa10ecda7
isso0424 <isso04240129@gmail.com>
2020-08-15 20:21:22 +0900

    [Add]Call homeView from main.dart.

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,4 +1,6 @@
 
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_app/controller/home.dart';
 
@@ -18,12 +20,14 @@ class HomePage extends StatelessWidget {
                 RaisedButton(
                   child: Text("Send"),
                   onPressed: () =>
-                      Navigator.of(context).pushNamed(controller.GetSendPage()),
+                      // Navigator.of(context).pushNamed(controller.GetSendPage()),
+                      log(controller.GetSendPage()),
                 ),
                 RaisedButton(
                   child: Text("Receive"),
                   onPressed: () =>
-                      Navigator.of(context).pushNamed(controller.GetReceivePage()),
+                      // Navigator.of(context).pushNamed(controller.GetReceivePage()),
+                      log(controller.GetReceivePage()),
                 )
               ],
             )

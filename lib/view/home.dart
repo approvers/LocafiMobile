98651f8d8d6557580e3ab06388f15dd55303bed2
isso0424 <isso04240129@gmail.com>
2020-08-15 20:06:00 +0900

    [Add]Create Home view.

---
+++ lib/view/home.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_app/controller/home.dart';
+
+class HomePage extends StatelessWidget {
+  final IHomeController controller;
+  HomePage({this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Text("Locafi Mobile"),
+            Row(
+              children: [
+                RaisedButton(
+                  child: Text("Send"),
+                  onPressed: () =>
+                      Navigator.of(context).pushNamed(controller.GetSendPage()),
+                ),
+                RaisedButton(
+                  child: Text("Receive"),
+                  onPressed: () =>
+                      Navigator.of(context).pushNamed(controller.GetReceivePage()),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

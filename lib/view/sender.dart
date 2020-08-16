fe89232b885484e086259ef81478a1c78ba586e2
isso0424 <isso04240129@gmail.com>
2020-08-16 18:39:49 +0900

    [Add]Add ReceiverSelector

--- lib/view/sender.dart
+++ lib/view/sender.dart
@@ -28,3 +28,55 @@ class _SenderPageState extends State<SenderPage> {
     );
   }
 }
+
+class _ReceiverSelector extends StatefulWidget {
+  final ISenderController controller;
+
+  _ReceiverSelector({this.controller});
+
+  @override
+  _ReceiverSelectorState createState() => _ReceiverSelectorState();
+}
+
+class _ReceiverSelectorState extends State<_ReceiverSelector> {
+  ISenderController controller;
+  static const initialValue = "(Please choose one)";
+  String selectedValue = initialValue;
+
+  _ReceiverSelectorState({this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: controller.getReceiverNames(),
+      builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+        if (snapshot.hasData) {
+          var cache = snapshot.data;
+          cache.insert(0, initialValue);
+          return selectorTemplate(cache);
+        }
+        return selectorTemplate([selectedValue]);
+      },
+    );
+  }
+
+  Widget selectorTemplate(List<String> contents) {
+    return DropdownButton(
+      value: selectedValue,
+      icon: Icon(Icons.arrow_downward),
+      iconSize: 24,
+      elevation: 16,
+      onChanged: (String newValue) {
+        setState(() {
+          selectedValue = newValue;
+        });
+      },
+      items: contents.map<DropdownMenuItem<String>>((value) {
+        return DropdownMenuItem(
+          value: value,
+          child: Text(value),
+        );
+      }),
+    );
+  }
+}

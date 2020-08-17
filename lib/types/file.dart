35cb9c379827c038db3b2b462d094e27aba29ff8
isso0424 <isso04240129@gmail.com>
2020-08-17 13:21:33 +0900

    [Add]Create originalFile class

--- lib/types/file.dart
+++ lib/types/file.dart
@@ -1,4 +1,25 @@
+import 'dart:io';
+
+class OriginalFile implements AbstractFile{
+  String fileName;
+  int fileSize;
+
+  OriginalFile(File file) {
+    fileName = createFileNameFromPath(file.path);
+    fileSize = file.lengthSync();
+  }
+
+  String createFileNameFromPath(String path) {
+    List<String> pathList = path.split("/");
+    return pathList[-1];
+  }
+
+  String getFileName() => fileName;
+
+  int getFileSize() => fileSize;
+}
+
 abstract class AbstractFile {
   String getFileName();
-  String getFileSize();
+  int getFileSize();
 }

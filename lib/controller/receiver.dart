37a21d516a681c48da66b53e77b360811589108b
isso0424 <isso04240129@gmail.com>
2020-09-07 16:04:07 +0900

    [Add]Create receiver controller

--- lib/controller/receiver.dart
+++ lib/controller/receiver.dart
@@ -1,4 +1,61 @@
+import 'package:locafi_mobile/model/receiver.dart';
 import 'package:locafi_mobile/types/file.dart';
+
+class ReceiverController<T extends AbstractFile> implements IReceiverController<T> {
+  IReceiverServerModel _receiverServerModel;
+  IReceiverFileSaverModel _receiverFileSaverModel;
+
+  List<T> receivedFiles = [];
+
+  ReceiverController(IReceiverServerModel serverModel, IReceiverFileSaverModel saverModel) {
+    this._receiverServerModel = serverModel;
+    this._receiverFileSaverModel = saverModel;
+  }
+
+  @override
+  bool deleteFile(int index) {
+    try {
+      final file = this.receivedFiles.removeAt(index);
+      return file != null;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  @override
+  Future<bool> finishServer() async =>
+      await this._receiverServerModel.finishServer();
+
+  @override
+  List<T> getFileList() => [...this.receivedFiles];
+
+  @override
+  Future<bool> saveFile(int index) async {
+    try {
+      final file = this.receivedFiles[index];
+
+      return this._receiverFileSaverModel.saveFile(file);
+    } catch(e) {
+      return false;
+    }
+  }
+
+  @override
+  Future<bool> startServer(String serverName) async {
+    return await this._receiverServerModel.startServer(serverName);
+  }
+
+  @override
+  Future<List<T>> waitFiles() async {
+    final receivedFiles = await this._receiverServerModel.getFileList();
+    receivedFiles.forEach((newFile) {
+      this.receivedFiles.add(newFile);
+    });
+
+    return receivedFiles;
+  }
+
+}
 
 abstract class IReceiverController<T extends AbstractFile> {
   List<T> getFileList();
@@ -7,7 +64,7 @@ abstract class IReceiverController<T extends AbstractFile> {
 
   Future<List<T>> waitFiles();
 
-  Future<bool> saveFile(T file);
+  Future<bool> saveFile(int index);
 
   Future<bool> startServer(String serverName);
 

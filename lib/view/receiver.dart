6ecb00ef33cbd5bd7fef2484b6b57420f9c96462
isso0424 <isso04240129@gmail.com>
2020-09-15 20:57:08 +0900

    [Add]Create AppBar

--- lib/view/receiver.dart
+++ lib/view/receiver.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:locafi_mobile/controller/receiver.dart';
 
 class ReceiverPage extends StatefulWidget {
@@ -10,9 +11,50 @@ class ReceiverPage extends StatefulWidget {
 }
 
 class _PageState extends State<ReceiverPage> {
+  final List<int> selectedFiles = [];
   @override
   Widget build(BuildContext context) {
     final serverName = ModalRoute.of(context).settings.arguments.toString();
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ファイル受け取り"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: saveSelectedFiles,
+          ),
+          IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: deleteSelectedFiles,
+          ),
+        ],
+      ),
+      body: FutureBuilder(
+        future: widget.controller.startServer(serverName),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.hasData) {
+            // TODO: Create Receiver Contents
+            return Container();
+          }
+          if (snapshot.hasError) {
+            return Text(snapshot.error);
+          }
+          return CircularProgressIndicator();
+        },
+      ),
+    );
+  }
+
+  void saveSelectedFiles() {
+    selectedFiles.sort();
+    selectedFiles.reversed.forEach((index) {
+      widget.controller.saveFile(index);
+    });
+  }
+  void deleteSelectedFiles() {
+    selectedFiles.sort();
+    selectedFiles.reversed.forEach((index) {
+      widget.controller.deleteFile(index);
+    });
   }
 }
